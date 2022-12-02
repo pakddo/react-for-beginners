43a79fc0022301c68b8ed756762824807fb931ef
Dooho Park <pakddo@pakddo.net>
2022-12-02 16:38:50 +0900

    7.2 Coin Tracker (Code Challenge)
    7.2 Coin Tracker (Code Challenge)
    - 입력된 수치로 몇개의 코인을 살수 있는지 표시

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { useEffect, useState } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [price, setPrice] = useState(0);
+  const [input, setInput] = useState(0);
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
       .then((response) => response.json())
@@ -11,19 +13,29 @@ function App() {
         setLoading(false);
       })
   }, [])
+  const onSelect = (event) => {setPrice(event.target.value)}
+  const onInput =  (event) => {setInput(event.target.value)}
+  console.log(input, price);
   return (
     <div>
-      <h1>The Coins! {loading ? "": `(${coins.length})`}</h1>
+      <h1>The Coins! {loading ? "": `(${coins.length})`}</h1> 
       {loading ? (
         <strong>Loading...</strong>
       ) : (
-        <select>
-          {coins.map((coin) => (
-            <option>
-              {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD
-            </option>
-          ))}
-        </select>
+        <div>
+          <label htmlFor="usd">USD:</label>
+          <input id="usd" placeholder="Input USD" type="number" onChange={onInput}/>
+          <h3>You can buy {input / price} </h3>
+          <select onChange={onSelect}>
+              <option value="none" selected disabled hidden>Select an Option</option>
+            {coins.map((coin) => (
+              <option value={coin.quotes.USD.price}>
+                {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD
+              </option>
+            ))}
+          </select>
+         
+        </div>
       )}
 
     </div>
