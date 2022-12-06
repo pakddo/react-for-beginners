8722c7f2d41597a241d64d2d2b3ac8ad1732e579
Dooho Park <pakddo@pakddo.net>
2022-12-06 15:19:40 +0900

    7.6 Parameters
    7.6 Parameters
    - route url에 paramter를 설정하여 다른 페이지로 전달
    - detail 페이지 표시 수정

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,5 +1,33 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 function Detail() {
-    return <h1>Detail</h1>
+    const { id } = useParams();
+    const [loading, setLoading] = useState(true);
+    const [movie, setMovie] = useState({});
+    const getMovie = async () => {
+        const json = await (
+            await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+        ).json();
+        setMovie(json.data.movie);
+        setLoading(false);
+    };
+    useEffect(() => {
+        getMovie();
+    })
+    return (
+        <div>
+            {loading ? (
+                <h1>Loading...</h1>
+            ) : (
+                <div>
+                    <h1>{movie.title_long}</h1>
+                    <img src={movie.medium_cover_image} alt={movie.title} />
+                    <p>{movie.description_intro}</p>
+                </div>
+            )}
+        </div>
+    )
 };
 
 export default Detail;
